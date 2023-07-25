9d34ad830ee040c566637ce0911aa10cd98a3f34
bronifty <bronifty@gmail.com>
2023-07-25 05:34:37 +0000

    this

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,52 +1,114 @@
-import React from "react";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+// export default function Game({ history }) {
+//   const navigate = useNavigate();
+//   const [score, setScore] = React.useState(0);
+//   const MAX_SECONDS = 5;
+//   const [ms, setMs] = React.useState(999);
+//   const [seconds, setSeconds] = React.useState(MAX_SECONDS);
+
+//   React.useEffect(() => {
+//     const currentTime = new Date();
+//     const interval = setInterval(() => updateTime(currentTime), 1);
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, []);
+
+//   const updateTime = (startTime) => {
+//     const endTime = new Date();
+//     const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+//     //add zeros if necessary to ensure the string has exactly 5 characters
+//     const formattedMSString = ("0000" + msPassedStr).slice(-5);
+//     //0000 - first 2 are the seconds, and the last 3 are the ms
+//     const updatedSeconds =
+//       MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
+//     const updatedMs =
+//       1000 -
+//       parseInt(formattedMSString.substring(formattedMSString.length - 3));
+//     setSeconds(addLeadingZeros(updatedSeconds, 2));
+//     setMs(addLeadingZeros(updatedMs, 3));
+//   };
+
+//   React.useEffect(() => {
+//     if (seconds <= -1) {
+//       navigate("/gameOver");
+//       //   history.push("/gameOver");
+//       //   window.history.pushState(null, null, "/highScores");
+//       //   window.location.pathname = "highScores";
+//     }
+//   }, [seconds, ms, navigate]);
+
+//   const addLeadingZeros = (str, length) => {
+//     let zeros = "";
+//     for (let i = 0; i < length; i++) {
+//       zeros += "0";
+//     }
+//     return (zeros + str).slice(-length);
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         Score: <strong>{score}</strong>
+//       </div>
+
+//       <div>A</div>
+//       <div>
+//         Time:{" "}
+//         <strong>
+//           {seconds}:{ms}
+//         </strong>
+//       </div>
+//     </>
+//   );
+// }
+
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Game({ history }) {
+export default function Game() {
   const navigate = useNavigate();
-  const [score, setScore] = React.useState(0);
+  const [score, setScore] = useState(0);
   const MAX_SECONDS = 5;
-  const [ms, setMs] = React.useState(999);
-  const [seconds, setSeconds] = React.useState(MAX_SECONDS);
+  const [time, setTime] = useState({ sec: MAX_SECONDS, ms: 0 });
 
-  React.useEffect(() => {
-    const currentTime = new Date();
-    const interval = setInterval(() => updateTime(currentTime), 1);
-    return () => {
-      clearInterval(interval);
-    };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime.ms === 0) {
+          if (prevTime.sec === 0) {
+            clearInterval(interval);
+            return prevTime;
+          } else {
+            return { sec: prevTime.sec - 1, ms: 99 };
+          }
+        } else {
+          return { sec: prevTime.sec, ms: prevTime.ms - 1 };
+        }
+      });
+    }, 10);
+
+    return () => clearInterval(interval);
   }, []);
 
-  const updateTime = (startTime) => {
-    const endTime = new Date();
-    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
-    //add zeros if necessary to ensure the string has exactly 5 characters
-    const formattedMSString = ("0000" + msPassedStr).slice(-5);
-    //0000 - first 2 are the seconds, and the last 3 are the ms
-    const updatedSeconds =
-      MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
-    const updatedMs =
-      1000 -
-      parseInt(formattedMSString.substring(formattedMSString.length - 3));
-    setSeconds(addLeadingZeros(updatedSeconds, 2));
-    setMs(addLeadingZeros(updatedMs, 3));
-  };
-
-  React.useEffect(() => {
-    if (seconds <= -1) {
+  useEffect(() => {
+    if (time.sec === 0 && time.ms === 0) {
       navigate("/gameOver");
-      //   history.push("/gameOver");
-      //   window.history.pushState(null, null, "/highScores");
-      //   window.location.pathname = "highScores";
     }
-  }, [seconds, ms, navigate]);
+  }, [time, navigate]);
 
-  const addLeadingZeros = (str, length) => {
-    let zeros = "";
-    for (let i = 0; i < length; i++) {
-      zeros += "0";
-    }
-    return (zeros + str).slice(-length);
+  const keyUpHandler = (e) => {
+    console.log(e.key);
   };
+
+  useEffect(() => {
+    document.addEventListener("keyup", keyUpHandler);
+    return () => {
+      document.removeEventListener("keyup", keyUpHandler);
+    };
+  }, []);
 
   return (
     <>
@@ -58,7 +120,7 @@ export default function Game({ history }) {
       <div>
         Time:{" "}
         <strong>
-          {seconds}:{ms}
+          {time.sec}:{time.ms < 10 ? `0${time.ms}` : time.ms}
         </strong>
       </div>
     </>
